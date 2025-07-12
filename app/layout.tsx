72254f2c734cbcab5bd16c3e5f2c42369584d2fd
bharani-palani <tp.bharani@gmail.com>
2025-07-12 20:05:45 +0530

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Suspense } from "react";
+import { Providers } from "./store/providers";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <Suspense fallback={<div>Loading...</div>}>
+      <html lang="en">
+        <body className={`antialiased`}>
+          <Providers>{children}</Providers>
+        </body>
+      </html>
+    </Suspense>
   );
 }
